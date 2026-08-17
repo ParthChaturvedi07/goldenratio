725f03f00ec57d11de77fbfd0c6c02ac8b6e17bb
Parth Chaturvedi <parthchaturvedi0204@gmail.com>
2026-08-17 18:56:35 +0530

    navbar reload

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,12 +1,8 @@
-"use client";
-
 import React from "react";
-
-import Link from "next/link";
 
 export default function Logo({ dark = false }: { dark?: boolean }) {
   return (
-    <Link
+    <a
       href="/"
       className={`relative z-50 text-[26px] md:text-[30px] font-bold tracking-tight leading-none transition-colors duration-500 ${dark ? "text-black" : "text-white"
         }`}
@@ -14,6 +10,6 @@ export default function Logo({ dark = false }: { dark?: boolean }) {
     >
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img src="/images/GoldenRatio_Creation.png" alt="Logo" className="w-24" />
-    </Link>
+    </a>
   );
 }
